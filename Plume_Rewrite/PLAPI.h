cd3af4443610c5f349f996ecb776498c5c76dafc
iEdon <admin@iedon.com>
2017-05-23 20:39:31 +0800

    更新PLAPI接口定义

--- Plume_Rewrite/PLAPI.h
+++ Plume_Rewrite/PLAPI.h
@@ -1,15 +1,26 @@
 #pragma once
+#include <iostream>
+
+#ifndef NULL
+#define NULL 0
+#endif
 #ifndef WINAPI
 #define WINAPI __stdcall
 #endif
+#define NEW(p, size) { p = (char *)malloc(size); memset( p, 0, size ); }
+#define DEL(p) { free((void*)p); p = NULL; }
+#ifndef MAX_PATH
+#define MAX_PATH 260
+#endif
+#ifndef MAKEWORD
+#define MAKEWORD(a, b) ((unsigned short)(((unsigned char)(a)) | ((unsigned short)((unsigned char)(b))) << 8))
+#endif
+
 #define PLAPI extern "C" __declspec(dllexport)
 #define PLAPI_ServerFunc bool (WINAPI *ServerFunc)(int dwServerID, int dwConnID, int dwAction, int dwFlag, void *lpvBuffer, int ilength)
 #define PLAPI_GetEnvVar bool(WINAPI *GetEnvVar)(int dwServerID, int dwConnID, const char *lpszVarName, void *lpvBuffer)
 #define PLAPI_Read bool(WINAPI *Read)(int dwServerID, int dwConnID, void *lpvBuffer, int dwBytesToRead, int *dwBytesRead)
 #define PLAPI_Write bool(WINAPI *Write)(int dwServerID, int dwConnID, const void *lpvBuffer, int ilength)
-#define NEW(p, size) { p = (char *)malloc(size); memset( p, 0, size ); }
-#define DEL(p) { free((void*)p); p = NULL; }
-#define MAX_PATH_LEN 1024
 
 #define PLUME_EVENT_BEGIN_CONN 1
 #define PLUME_EVENT_DONE_CONN 2
@@ -25,6 +36,7 @@
 #define PLUME_ACTION_READ_CONFIG 3
 #define PLUME_ACTION_SEND_HEADER 4
 #define PLUME_ACTION_DONE_REQUEST 5
+#define PLUME_ACTION_GET_CONNECTION_COUNTS 6
 
 #define PLUME_PROC_RESULT_SUCCESS 1
 #define PLUME_PROC_RESULT_FAILURE 0
@@ -37,13 +49,22 @@ typedef struct plume_server
 	PLAPI_Read;
 	PLAPI_Write;
 	PLAPI_ServerFunc;
-};
+} plume_server;
+
 typedef struct plume_path_struct
 {
 	char* pConnDIR;
 	char* pPathTranslated;
 	char* pPath;
-};
+} plume_path_struct;
+
+typedef struct plume_readconfig_struct
+{
+	char* pSectionName;
+	char* pKeyName;
+	char* pValue;
+} plume_readconfig_struct;
+
 PLAPI bool WINAPI Plume_InitPlugin(int dwServerVersion, PLAPI_ServerFunc);
 PLAPI int WINAPI Plume_PluginProc(plume_server * plume_server);
 PLAPI void WINAPI Plume_GetPluginDesc(char **lpszName, char **lpszVersion, char **lpszAuthor, char **lpszDescription);
